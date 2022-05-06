e52fa9342643693f4b1494c7611815829925d221
Felipe <felipe.seraf@gmail.com>
2022-05-06 20:28:58 -0300

    First Commit

---
+++ src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import { MailAdapter, SendMailData } from "../mail-adapter";
+import nodemailer from 'nodemailer';
+
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+        user: "57d716701a0c2f",
+        pass: "904f738f79f2ad"
+    }
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+    async sendMail({ subject, body }: SendMailData) {
+        await transport.sendMail({
+            from: 'Equipe Feedget <oi@feedget.com>',
+            to: 'Felipe Serafim <felipe.seraf@gmail.com>',
+            subject,
+            html: body
+        });
+    };
+}
